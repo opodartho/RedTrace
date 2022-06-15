86614872093878999865b6880000f0cb8911de9f
Zahidul Haque <iisowrovii@gmail.com>
2022-06-16 02:38:10 +0600

    refactor test code

--- spec/controllers/public/companies_controller_spec.rb
+++ spec/controllers/public/companies_controller_spec.rb
@@ -3,17 +3,14 @@ require 'rails_helper'
 RSpec.describe Public::CompaniesController do
   describe '#create' do
     context 'when the company or manager is invalid' do
-      let(:invalid_param) {
-        {
+      it 're-renders the form' do
+        invalid_param = {
           company_form: {
             terms_of_service: '0',
             company_attributes: attributes_for(:company),
             user_attributes: attributes_for(:user)
           }
         }
-      }
-
-      it 're-renders the form' do
         post :create, params: invalid_param
 
         expect(response).to render_template :new
--- spec/forms/company_form_spec.rb
+++ spec/forms/company_form_spec.rb
@@ -2,17 +2,14 @@ require 'rails_helper'
 
 RSpec.describe CompanyForm do
   describe '#submit' do
-    subject {
-      {
-        terms_of_service: true,
-        company_attributes: attributes_for(:company),
-        user_attributes: attributes_for(:user),
-      }
-    }
-
     context 'create new company and it\'s manager' do
       it 'with valid params' do
-        company_form = described_class.new(subject)
+        param = {
+          terms_of_service: true,
+          company_attributes: attributes_for(:company),
+          user_attributes: attributes_for(:user),
+        }
+        company_form = described_class.new(param)
 
         expect { company_form.submit }.to change { Company.count }.by(1).and change { User.count }.by(1)
       end
@@ -20,15 +17,23 @@ RSpec.describe CompanyForm do
 
     context 'return errors' do
       it 'without agree to terms of service' do
-        invalid_subject = subject.merge(terms_of_service: false)
-        company_form = described_class.new(invalid_subject)
+        invalid_param = {
+          terms_of_service: false,
+          company_attributes: attributes_for(:company),
+          user_attributes: attributes_for(:user),
+        }
+        company_form = described_class.new(invalid_param)
 
         expect(company_form).to be_invalid
       end
 
       it 'without valid manager information' do
-        invalid_subject = subject.merge(user_attributes: attributes_for(:user, msisdn: nil))
-        company_form = described_class.new(invalid_subject)
+        invalid_param = {
+          terms_of_service: false,
+          company_attributes: attributes_for(:company),
+          user_attributes: attributes_for(:user, msisdn: nil),
+        }
+        company_form = described_class.new(invalid_param)
 
         expect(company_form).to be_invalid
       end
