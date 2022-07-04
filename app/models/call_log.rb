85acff2e96d94dcfb0a15c6135da2068190d1d5d
Ananya <ananyakazi92@gmail.com>
2022-07-04 23:10:42 +0600

    - copy pasted locations test codes to call logs

--- app/models/call_log.rb
+++ app/models/call_log.rb
@@ -2,5 +2,5 @@ class CallLog < ApplicationRecord
   acts_as_tenant :company
   belongs_to :user
 
-  validates :msisdn, :call_type, duration, start_time, end_time, presence: true
+  validates :msisdn, :call_type, :duration, :start_time, :end_time, presence: true
 end
